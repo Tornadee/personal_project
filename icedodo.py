767e46f6c8d1248ceb9c10852418f70145d25e37
jason <flyingdodo9000@gmail.com>
2018-10-19 11:49:55 +0700

    ok

--- icedodo.py
+++ icedodo.py
@@ -1,36 +1,84 @@
-from threading import Timer
+# © Jason (Seojoon) Yeon 
+# 2018 October 17 ~ 
+
 import random
 import math
-import keras
+import numpy as np
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
 class AI:
 	def __init__(self):
-		self.num_states = 5;
-		self.num_actions = 3;
+		self.num_states = 6; # px, pz, ax, az, bx, bz
+		self.num_actions = 3; # -1 or 0 or 1
+		self.batch_size = 32;
+		self.data = [];
+		self.memory = 50000;
 
 		self.alpha = 0.01;
+		self.gamma = 0.99;
 		self.epsilon = 1.00;
 		self.ep_decay = 0.994;
-		self.model = _create_model();
+		self.min_ep = 0.02;
+		self.model = self._create_model();
 	
 	def _create_model(self):
 		model = Sequential();
+		model.add(Dense(24, input_shape=(self.num_states,), activation="relu"))
+		model.add(Dense(24, activation="relu"))
+		model.add(Dense(self.num_actions, activation="linear"))
+		adam = Adam(lr=self.alpha);
+		model.compile(adam, loss="mean_squared_error");
 		return model;
 
-	def _choose_action(self, data):
+	def _choose_action(self, state):
 		if (random.random() <= self.epsilon):
-
+			return (random.random() * self.num_actions) - 1;
 		else:
-			prediction = _argmax_predict(data);
+			pred = self.model.predict(np.array([state]))[0];
+			arg_max = np.argmax(pred);
+			return arg_max
 
-	def _argmax_predict(self, data)
-		pred = self.model.predict(data);
-		return pred # argmax this.
+	def _replay(self):
+		sample = self._sample(self.data, self.batch_size);
+		for state, action, next_state, reward, done in sample:
+			# new Q-table = 
+			# Current Q-table +
+			q_current = self.model.predict(np.array([state]));
+			# discount rate (gamma) * max_possible Q
+			q_future = self.model.predict(np.array([next_state]))[0];
+			max_q = q_future[np.argmax(q_future)];
+			# new Q_s_a =
+			q_new = q_current + self.alpha * (reward + (self.gamma * max_q) - q_current)
+			# assignment
+			q_current[action] = q_new
+			xs = np.array()
+			ys = np.array()
+			self.model.fit(xs, ys, epochs=1);
 
-	def _predict(self, data)
-		pred = self.model.predict(data);
-		return pred
+	def _predict(self, state):
+	    pred = self.model.predict(np.array(state));
+	    return pred;
 
+
+	def _sample(self, data, count):
+		if (count > len(data)):
+			return False;
+		else:
+			sample = [];
+			sampled_ids = [];
+			while len(sample) < count:
+				rand_id = random.randint(0, count-1);
+				if rand_id not in sampled_ids:
+					sample.append(data[rand_id]);
+					sampled_ids.append(rand_id);
+			return sample;
+
+	def _add_to_memory(self, data):
+		self.data.append(data);
+		if (len(self.data)) > self.memory:
+			self.data.pop(0);
 
 class player:
 	def __init__(self):
@@ -48,6 +96,8 @@ class game:
 		self.player = player();
 		self.alive = True;
 		self.frame = 0;
+		self.state = self._return_state();
+
 		while self.alive:
 			self.frame += 1;
 			if (self.frame % 1000) == 0:
@@ -61,11 +111,22 @@ class game:
 			self.platforms.pop(0);
 
 		# this part will be controlled by the AI.
-		a_x, a_z, b_x, b_z = self._get_upcoming_blocks_pos();
-		data = (self.player.x, self.player.z, a_x, a_z, b_x, b_z);
-		action = AI._choose_action(data);
+		action = AI._choose_action(self.state);
 		self.player.r += action;
-		self._move_player();
+		# data collection part
+		done = self._move_player();
+		next_state = self._return_state();
+		# Q-learning
+		reward = 1 if not done else -10
+		AI._add_to_memory([self.state, action, next_state, reward, done])
+		if (len(AI.data) >= AI.batch_size):
+			AI._replay();
+		# pass on the state
+		self.state = next_state;
+		# epsilon effect
+		if (len(AI.data) > AI.batch_size):
+			if (AI.epsilon > AI.min_ep):
+				AI.epsilon *= AI.ep_decay;
 
 	def _move_player(self):
 		self.player.x += math.sin(self.player.r) * 0.4;
@@ -73,9 +134,12 @@ class game:
 		if (self._is_player_on_platform() == False):
 			self.alive = False;
 			print("score: {}".format(str(round(self.player.z))));
+			return True;
+		else:
+			return False;
 
 	def _build_platform(self):
-		shift_x = random.randint(-1,1);
+		shift_x = random.randint(-0,0);
 		shift_z = 5;
 		x = self.previous_platform_x + shift_x;
 		z = self.previous_platform_z + shift_z;
@@ -103,15 +167,23 @@ class game:
 				break;
 		return a;
 
-	def _get_upcoming_blocks_pos(self):
-		a_x, a_z, b_x, b_z = 0,0,0,0;
+	def _return_state(self):
 		a_index = None;
 		for i in range(len(self.platforms)):
 			platform = self.platforms[i];
 			if (platform.z > self.player.z + 1):
-				a_index = 
-		
-		return a_x, a_z, b_x, b_z;
+				# player position
+				p_x = self.player.x;
+				p_z = self.player.z;
+				# closest next platform, called "a"
+				a_x = platform.x;
+				a_z = platform.z;
+				# second next platform, called "b"
+				b_x = self.platforms[i+1].x;
+				b_z = self.platforms[i+1].z;
+				a_index = i;
+				return p_x, p_z, a_x, a_z, b_x, b_z;
 
 if __name__ == "__main__":
+	AI = AI();
 	game = game();
